53d0c1de148a7241669701898f06b9ad7628785f
ivansanguezax <ivansanguezax@gmail.com>
2023-05-24 10:40:36 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <title>InfoApp</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-grad-school.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+<!--
+    
+TemplateMo 557 Grad School
+
+https://templatemo.com/tm-557-grad-school
+
+-->
+  </head>
+
+<body>
+
+   
+  <!--header-->
+  <header class="main-header clearfix" role="header">
+    <div class="logo">
+      <a href="index.php"><em>Info</em> App</a>
+    </div>
+    <a href="#menu" class="menu-link"><i class="fa fa-bars"></i></a>
+    <nav id="menu" class="main-nav" role="navigation">
+      <ul class="main-menu">
+        <li><a href="index.php">Inicio</a></li>
+        <li class="has-submenu"><a href="#section2">Sobre Nosotros</a>
+          <ul class="sub-menu">
+            <li><a href="#section2">Malla</a></li>
+            <li><a href="#section3">Eventos</a></li>
+            <li><a href="#section3">FCPN</a></li>
+          </ul>
+        </li>
+        <li><a href="semestres.php">Semestres</a></li>
+        <!-- <li><a href="#section5">Video</a></li> -->
+        <li> <div class="main-button">
+          <div class="scroll-to-section"><a href="login.php">Empezar</a></div>
+        </div></li>
+        
+
+      </ul>
+    </nav>
+  </header>
---
+++ index.php
@@ -0,0 +1,90 @@
+
+<?php include 'header.php'; ?>
+
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <section class="section main-banner" id="top" data-section="section1">
+      <video autoplay muted loop id="bg-video">
+          <source src="assets/images/course-video.mp4" type="video/mp4" />
+      </video>
+
+      <div class="video-overlay header-text">
+          <div class="caption">
+              <h6>Facultad de Ciencias Puras y Naturales</h6>
+              <h2><em>Tu</em> Classroom </h2>
+              <div class="main-button">
+                  <div class="scroll-to-section"><a href="#section2">Empezar</a></div>
+              </div>
+          </div>
+      </div>
+  </section>
+  <!-- ***** Main Banner Area End ***** -->
+
+
+  
+
+  <section class="section why-us" data-section="section2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Selecciona tu grado</h2>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div id='tabs'>
+            <ul>
+              <li><a href='#tabs-1'>Pre-facultativo</a></li>
+              <li><a href='#tabs-2'>Universitario</a></li>
+              <li><a href='#tabs-3'>Post-grado</a></li>
+            </ul>
+            <section class='tabs-content'>
+              <article id='tabs-1'>
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src="assets/images/choose-us-image-01.png" alt="">
+                  </div>
+                  <div class="col-md-6">
+                    <h4>Pre-facultativo</h4>
+                    <p>El programa Pre-facultativo ofrece una sólida preparación académica para aquellos que deseen ingresar a la universidad. Contamos con un equipo de profesionales dedicados y recursos de calidad para garantizar tu éxito en los exámenes de admisión.</p>
+                  </div>
+                </div>
+              </article>
+              <article id='tabs-2'>
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src="assets/images/choose-us-image-02.png" alt="">
+                  </div>
+                  <div class="col-md-6">
+                    <h4>Universitario</h4>
+                    <p>Nuestra oferta universitaria te brinda una educación de primer nivel con una amplia variedad de programas y especialidades. Contamos con profesores altamente capacitados, instalaciones modernas y oportunidades de aprendizaje experiencial para prepararte de manera integral para tu futuro profesional.</p> 
+                    <p>Además, ofrecemos opciones de intercambio estudiantil y prácticas en empresas líderes en el mercado, brindándote una ventaja competitiva en el mundo laboral.</p>
+                  </div>
+                </div>
+              </article>
+              <article id='tabs-3'>
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src="assets/images/choose-us-image-03.png" alt="">
+                  </div>
+                  <div class="col-md-6">
+                    <h4>Post-grado</h4>
+                    <p>Nuestros programas de postgrado te ofrecen la oportunidad de especializarte en tu campo de estudio y alcanzar el más alto nivel académico. Trabajarás de cerca con investigadores de renombre y tendrás acceso a recursos avanzados que te permitirán realizar contribuciones significativas en tu área de expertise.</p>
+                    <p>Además, fomentamos la colaboración y el networking entre nuestros estudiantes de postgrado, brindándote la oportunidad de establecer conexiones duraderas y enriquecedoras con profesionales de todo el mundo.</p>
+                  </div>
+                </div>
+              </article>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+
+  
+
+
+
+  <?php include 'footer.php'; ?>
